15d3602868ccf85672bb162401505625a841ca95
atharvgurav1665 <atharvgurav1665@gmail.com>
2026-06-19 09:48:31 +0530

    accesstokenp1

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,6 +6,11 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import mongoose from "mongoose";
 
 
+const generateAccessAndRefreshTokens = {
+    
+}
+
+
 const registerUser = asyncHandler( async (req,res) =>{
     //steps for registation of user --->>>
     //
@@ -92,5 +97,45 @@ const registerUser = asyncHandler( async (req,res) =>{
 
 })
 
-export {registerUser}
+const loginUser = asyncHandler(async (req,res) => {
+    // req body -> data 
+    // username or email 
+    // find the user
+    // password check 
+    // access and refresh token 
+    //send cookies
+
+
+    const {email , username,password} = req.body
+    
+    if(!username || !email) {
+        //agar user ke pass username or email dono na ho to ye error ayega in return ...
+        throw new ApiError(400,"username or passowrd is required ")
+    }
+
+    
+    //"To find the user " from  data base we use both  the options here username or email using following $or operator and findOne() function -->> 
+    const user = await User.findOne({
+        $or : [{username},{email}]
+    })
+
+    if(!user) {
+        throw new ApiError(404,"User Doesnot exist !")
+    }
+
+    //checking thepassword -->>  User [ mongo DB user ]  user [ our user so find or check using this lowercase user ]
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if(!isPasswordValid){
+        throw new ApiError(401,"Invalid user credentials  !")
+    }
+
+    
+
+})
+
+export {
+    registerUser,
+    loginUser,
+}
 
