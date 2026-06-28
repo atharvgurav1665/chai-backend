3ec97b3e9c8790392ee2f5753aaf5e4ea4bf148a
atharvgurav1665 <atharvgurav1665@gmail.com>
2026-06-28 14:53:11 +0530

    sub pipelines

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -443,7 +443,53 @@ const getUserChannelProfile = asyncHandler(async(req,res) => {
 })
 console.log(channel);
 
+const getWatchHistory = asyncHandler(async(req,res)=>{
+    const user = await User.aggregate([
+        {
+            $match:{
+                _id : new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup : {
+                from:"videos",
+                localField : "watchHistory",
+                foreignField:"_id",
+                as :"watchHistory",
+                pipeline:[
+                    {
+                        $lookup : "users",
+                        localField : "owner",
+                        foreignField : "_id",
+                        as : "owner",
+                        pipeline : [
+                            {
+                                $project : {
+                                    fullName : 1,
+                                    username : 1,
+                                    avatar : 1
+                                }
+                            }
+                        ]
+                    },
+                    {
+                        $addFields: {
+                            owner : {
+                                $first :"$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
 
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,user[0].watchHistory,"watch Hostory Fetched SuccessFully ")
+    )
+})
 //exporting all functions -->> 
 export {
     registerUser,
@@ -456,5 +502,6 @@ export {
     updateUserAvatar,
     updateUserCoverImage,
     getUserChannelProfile,
+    getWatchHistory,
 }
 
